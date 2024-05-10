01a80880ef30d124d80d825f3ce54cadb8821451
Anusha <anushavkhandke@gmail.com>
2024-05-10 21:14:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const jsonData = [
+    { "userId": "rirani", "jobTitleName": "Developer", "firstName": "Romin", "lastName": "Irani", "preferredFullName": "Romin Irani", "employeeCode": "E1", "region": "CA", "phoneNumber": "408-1234567", "emailAddress": "romin.k.irani@gmail.com" },
+    { "userId": "nirani", "jobTitleName": "Developer", "firstName": "Neil", "lastName": "Irani", "preferredFullName": "Neil Irani", "employeeCode": "E2", "region": "CA", "phoneNumber": "408-1111111", "emailAddress": "neilrirani@gmail.com" },
+     { "userId": "thanks", "jobTitleName": "Program Directory", "firstName": "Tom", "lastName": "Hanks", "preferredFullName": "Tom Hanks", "employeeCode": "E3", "region": "CA", "phoneNumber": "408-2222222", "emailAddress": "tomhanks@gmail.com" }, 
+     { "userId": "Vikram", "jobTitleName": "Program Directory", "firstName": "Tom", "lastName": "Hanks", "preferredFullName": "Tom Hanks", "employeeCode": "E3", "region": "CA", "phoneNumber": "408-2222222", "emailAddress": "vikram@gmail.com" },
+      { "userId": "Lokesh", "jobTitleName": "Program Directory", "firstName": "Tom", "lastName": "Hanks", "preferredFullName": "Tom Hanks", "employeeCode": "E3", "region": "CA", "phoneNumber": "408-2222222", "emailAddress": "lokesh@gmail.com" },
+       { "userId": "Sateesh", "jobTitleName": "Program Directory", "firstName": "Tom", "lastName": "Hanks", "preferredFullName": "Tom Hanks", "employeeCode": "E3", "region": "CA", "phoneNumber": "408-2222222", "emailAddress": "sateesh@gmail.com" }
+    ];
